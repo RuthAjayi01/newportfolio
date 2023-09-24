a87ebf4d20d489bd9067882fd46c001dcc861e8a
RuthAjayi01 <143891741+RuthAjayi01@users.noreply.github.com>
2023-09-24 16:18:45 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// JavaScript to handle mobile menu toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
